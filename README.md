f103deaa57693e4ca017dcb4c1894f1c04e8ac7f
LRawlings <lukerawlings01@gmail.com>
2022-10-30 15:31:05 -0500

    added files

---
+++ README.md
@@ -0,0 +1,24 @@
+# EveryDollar Scraper
+
+Totals the 'Remaining' column from the EveryDollar budgeting web app. 
+
+## Input
+An html file of EveryDollar's budgeting tool.
+
+## Output
+A csv of each budget item's remaining amount, with a total at the end.
+
+## Example console
+
+```
+Now starting scraper script
+budgetItemRows has 5 elements.
+[0] Rent: 1000.00. totalRemaining is now: 1000
+[1] Grocery: 0.00. totalRemaining is now: 1000
+[2] Utilities: 11.00 totalRemaining is now: 1011
+[3] Transportation: 15.50. totalRemaining is now: 1026.50
+[4] Savings: 3000. totalRemaining is now: 4026.50
+End of loop. totalRemaining: 4026.50
+csv output written to everydollar-remaining-1667158846401.csv
+End of scraper script
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const DomParser = require('dom-parser');
+const ExactMath = require('exact-math');
+
+
+console.log("Now starting scraper script");
+var parser = new DomParser();                                           // Initialize dom parser
+const fileContents = fs.readFileSync('./EveryDollar.htm').toString()    // Read file to string
+const dom = parser.parseFromString(fileContents);                       // Parse string to dom object
+const array = [["Budget Item", "Amount Remaining"]];                    // Declare array
+
+const budgetItemRows = dom.getElementsByClassName("BudgetItemRow--remainingAllocationPreview"); // Get the elements I'm interested in
+console.log(`budgetItemRows has ${budgetItemRows.length} elements.`);
+let totalRemaining = "0";
+
+for(let i = 0; i < budgetItemRows.length; i++) { // Loop through each budget item
+    const row = budgetItemRows[i];
+    let label = "";
+    let value = "";
+
+    const labelElement = row.getElementsByClassName("BudgetItem-label")[0];     // Get the label
+    const moneyInteger = row.getElementsByClassName("money-integer")[0];        // Get amount remaining
+    const moneyFractional = row.getElementsByClassName("money-fractional")[0];
+    
+
+    if(!moneyInteger) {
+        console.log(`[${i}] skipping because moneyInteger object is: ${moneyInteger}`);
+        continue;
+    }
+    if(!moneyFractional) {
+        console.log(`[${i}] skipping because moneyFractional object is: ${moneyFractional}`);
+        continue;
+    }
+    if(!labelElement) {
+        console.log(`[${i}] skipping because labelElement object is: ${labelElement}`);
+        continue;
+    }
+
+    value = `${moneyInteger.innerHTML}.${moneyFractional.innerHTML}`;
+    value = value.replace(/,/g, '');
+
+    try {
+        ExactMath.add(totalRemaining, value);
+    } catch (e) {
+        console.log(`[${i}] Failed to add value: ${value} which may not be a number. Error: ${e}`);
+        continue;
+    }
+
+    for(const attribute of labelElement.attributes) {
+        if(attribute.name === 'data-text')
+            label = attribute.value;
+    }    
+
+    totalRemaining = ExactMath.add(totalRemaining, value);
+
+    console.log(`[${i}] ${label} remaining: ${value}. totalRemaining is now: ${totalRemaining}`);
+    array.push([label, value]);
+}
+console.log(`End of loop. totalRemaining: ${totalRemaining}`);
+
+array.push(["Total",totalRemaining]);
+
+const csvStr = buildCsvStr(array);
+const filename = `everydollar-remaining-${Date.now()}.csv`;
+fs.writeFileSync(filename, csvStr);
+console.log(`csv output written to ${filename}`);
+
+console.log("End of scraper script");
+
+
+
+function buildCsvStr(array) { 
+// const buildCsvStr = (array) => {
+    let str = "";
+    for(const innerArray of array) {
+        for(let i = 0; i<innerArray.length; i++) {
+            str = str + innerArray[i];
+            if((i+1)<innerArray.length)
+                str = str + ","
+        }
+        str = str + "\n";
+    }
+    return str;
+}
